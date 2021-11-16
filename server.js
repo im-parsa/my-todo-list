586e23d60e188623d4f095e1ca3410bdebd6a343
Parsa <parsa.firoozi55@gmail.com>
2021-11-16 23:05:08 +0330

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const moment = require("moment"),
     chalk = require('chalk'),
-    dotEnv = require('dotenv'),
     connectDB = require("./config/mongodb.service");
 
 console.log('|' + chalk.blue(' ✅  ') + chalk.black.bgBlue(moment().format(' h:mm:ss a ')) + ' - ' + chalk.blue(moment().locale("en").format('MMMM Do YYYY')));
-
-dotEnv.config({ path: "./config/config.env" });
 
 const app = require('./app.service');
 
